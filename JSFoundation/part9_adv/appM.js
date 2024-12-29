14a4dceb56eaf8bdd6137d7e34370241db03ca62
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2024-12-29 19:49:30 +0530

    added commonJS

---
+++ JSFoundation/part9_adv/appM.js
@@ -0,0 +1,9 @@
+// default import
+import multiply from "./mathOperationsM";
+
+// named import
+import { add, subtract } from "./mathOperationsM";
+
+console.log(multiply(3, 5));
+console.log(add(3, 5));
+console.log(subtract(3, 5));
