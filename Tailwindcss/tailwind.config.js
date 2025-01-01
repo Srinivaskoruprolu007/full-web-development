0099684b39c8663fc318e888bd8c7e0cfc84874d
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-01 21:32:39 +0530

    added darkmode

--- Tailwindcss/tailwind.config.js
+++ Tailwindcss/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html, js}'],
+  content: ["./src/**/*.{html, js}"],
+  darkMode: "selector",
   theme: {
-    colors:{
-      brand:'#3498db'
+    colors: {
+      brand: "#3498db",
     },
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
