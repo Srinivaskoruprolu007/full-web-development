c54e688de8f888812932aa6d5b2ca09a61626d1f
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-16 12:27:35 +0530

    add tailwindcustom props

---
+++ Learn-react/02tailwindcssprops/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="bg-slate-400 flex flex-col justify-center items-center min-h-screen">
+      <h1 className="text-red-500 font-bold text-center">Hello every one</h1>
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
