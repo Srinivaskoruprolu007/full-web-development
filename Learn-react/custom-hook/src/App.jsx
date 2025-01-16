5dd020cb3590bc988e0696ba82f25c2d18165dd9
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-16 12:26:40 +0530

    add a custom hook project

---
+++ Learn-react/custom-hook/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import useCurrency from "./hooks/use-currency";
+import { InputBox } from "./components/Index";
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const data = useCurrency(from);
+  const options = Object.keys(data);
+
+  const swap = () => {
+    setTo(from);
+    setFrom(to);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+  const convert = () => {
+    setConvertedAmount(amount * data[to]);
+  };
+  return (
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-gray-500">
+      <div className="w-full">
+        <div
+          className="w-full max-w-md mx-auto border border-gray-200 rounded-lg p-5 backdrop-blur-sm
+      bg-white/30"
+        >
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label={from}
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                onAmountChange={(amount) => setAmount(amount)}
+                selectedCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                className="bg-blue-600 px-4 py-2 rounded-lg outline-none text-white hover:scale-105
+              hover:bg-blue-800 absolute left-1/2 -translate-x-1/2 -translate-y-1/2"
+                onClick={swap}
+              >
+                Swap
+              </button>
+            </div>
+            <div className="w-full mt-1">
+              <InputBox
+                label={to}
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                onAmountChange={(amount) => setConvertedAmount(amount)}
+                selectedCurrency={to}
+                amountDisabled
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className="w-full bg-green-600 m-2 rounded-lg px-2 py-1 text-white"
+                onSubmit={convert}
+              >{`Convert ${from.toUpperCase()} To ${to.toUpperCase()}`}</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Learn-react/custom-hook/src/components/InputBox.jsx
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import { useId } from "react";
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  className = "",
+}) => {
+  const id = useId();
+  return (
+    <div className={` bg-white p-3 rounded-lg  text-sm  flex ${className}`}>
+      <div className="w-1/2">
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
+        <input
+          id={id}
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 fle flex-wrap justify-end text-right">
+        <p className="mb-2 text-gray-400">Currency Type</p>
+        <select
+          className="rounded-lg py-1 px-1 bg-gray-300 cursor-pointer outline-none"
+          value={selectedCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+InputBox.propTypes = {
+  label: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  onAmountChange: PropTypes.func.isRequired,
+  onCurrencyChange: PropTypes.func.isRequired,
+  currencyOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedCurrency: PropTypes.string,
+  amountDisabled: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default InputBox;
